2473a121f0ea182e014caf6a1e4b087a51c6d75a
puneethx <puneethreddyt2004@gmail.com>
2025-03-20 23:53:31 +0530

    modified changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import "./App.scss"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import What from './components/What/What'
+
 import Why from './components/Why/Why';
+import What from './components/What/what';
 import How from "./components/How/How";
 import Whatsnew from "./components/Whatsnew/Whatsnew";
 import Signin from './components/Signin/Signin';
