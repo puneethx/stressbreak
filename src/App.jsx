35efe8fbccb3f6cc5e9113f95a242c198771dfb1
puneethx <puneethreddyt2004@gmail.com>
2025-03-21 00:08:35 +0530

    added scroll refresh effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import "./App.scss"
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 
 import Why from './components/Why/Why';
 import How from "./components/How/How";
@@ -10,9 +10,22 @@ import Signin from './components/Signin/Signin';
 
 
 function App() {
+  function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    }, [pathname]);
+
+    return null;
+  }
   return (
     <BrowserRouter>
       <>
+        <ScrollToTop />
         <div className='main'>
           <Routes>
             <Route path="/" element={<What />}> </Route>
@@ -20,7 +33,7 @@ function App() {
             <Route path="/why" element={<Why />}> </Route>
             <Route path="/how" element={<How />}> </Route>
             <Route path="/whats-new" element={<Whatsnew />}> </Route>
-            <Route path="/signin" element={<Signin/>}></Route>
+            <Route path="/signin" element={<Signin />}></Route>
           </Routes>
         </div>
       </>
